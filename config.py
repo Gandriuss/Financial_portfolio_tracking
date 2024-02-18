626b5e58dc705bdb6305498fd52a739e930db3f6
Gandriuss <99821822+Gandriuss@users.noreply.github.com>
2024-02-18 19:33:24 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-DATABASE_URI = 'postgresql://postgres:password@localhost/postgres'
+DATABASE_URI = 'postgresql://postgres:password@localhost/db_name'
 BASE_DIRECTORY = 'Your_direcotory'
 TOKEN = 'telegram_chatbot_token'
 CHAT_ID = 'telegram_chat_id'
